fc901355b90219daeb14f3c07baca8495ac5daea
Sangsu Song <dvlprsong@gmail.com>
2022-09-13 22:47:35 +0900

    TEMP: add nestedMap generator function

--- index.ts
+++ index.ts
@@ -1,7 +1,11 @@
+import { generateNestedMap } from './src/mapVsObject';
 import { samplePerformanceTest } from './src/samplePerformanceTest';
+import { fruits } from './src/utils/constants';
 
 function main() {
-  samplePerformanceTest();
+  // samplePerformanceTest();
+  const nestedMap = generateNestedMap(fruits);
+  console.log(nestedMap);
 }
 
 main();
